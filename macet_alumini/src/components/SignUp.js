39a05b456ffc8914fc181de18a18fccfd6bf8a25
tanweer005 <tanweer005@gmail.com>
2024-12-07 20:51:52 +0530

    add signUp

---
+++ macet_alumini/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to SignUp</h1>
+            </div>
+        );
+    }
+}
